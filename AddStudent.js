d5065a20073c7bd55f7aea8d037b96594e0a6d93
Abdelrhman R,Muhammed <11410120210225@stud.cu.edu.eg>
2023-04-10 19:16:15 +0200

    add js of page AddStudent

--- AddStudent.js
+++ AddStudent.js
@@ -1,20 +1,44 @@
-const form = document.getElementById('form');
+// Wait for the document to be loaded
+document.addEventListener("DOMContentLoaded", function(event) { 
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault(); // prevent the form from submitting to the server
+    // Get the studentForm element
+    const studentForm = document.getElementById('form');
+  
+    // Add an event listener for the form submit
+    studentForm.addEventListener('submit', function(event) {
+      event.preventDefault(); // prevent the default form submission
+  
+      // Get the form values
+      const name = document.getElementById('Sname').value;
+      const id = document.getElementById('Sid').value;
+      const email = document.getElementById('Semail').value;
+      const gpa = document.getElementById('Sgpa').value;
+      const nationalId = document.getElementById('Snational-id').value;
+      const address = document.getElementById('Saddress').value;
+      const phone = document.getElementById('Sphone').value;
+      const department = "General";
+      const date = document.getElementById('Sdate').value;
+      const level = document.querySelector('input[name="Slevel"]:checked').value;
+      const gender = document.querySelector('input[name="Sgender"]:checked').value;
+      const status = document.querySelector('input[name="Sstatus"]:checked').value;
 
-    const name = document.getElementById('Sname').value;
-    const id = document.getElementById('Sid').value;
-    const email = document.getElementById('Semail').value;
-    const gpa = document.getElementById('Sgpa').value;
-    const nationalId = document.getElementById('Snational-id').value;
-    const address = document.getElementById('Saddress').value;
-    const phone = document.getElementById('Sphone').value;
-    const department = document.getElementById('Sdepartment').value;
-    const date = document.getElementById('Sdate').value;
-    const level = document.querySelector('input[name="Slevel"]:checked').value;
-    const gender = document.querySelector('input[name="Sgender"]:checked').value;
-    const status = document.querySelector('input[name="Sstatus"]:checked').value;
-
-    console.log(name, id , email,gpa , nationalId , address , phone , department , date , level , gender , status);
-});
+      // Create an object with the form data
+      const formData = {
+        name,
+        id,
+        email,
+        gpa,
+        nationalId,
+        address,
+        date,
+        department,
+        phone,
+        level,
+        status,
+        gender
+      };
+      console.log(formData);
+      studentForm.reset();
+    });
+  });
+  
