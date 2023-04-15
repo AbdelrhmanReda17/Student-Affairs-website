f0dc42c42ea0af75d9fc6f20211898825764bda4
Hoda Touny <100847847+HodaTouny@users.noreply.github.com>
2023-04-15 19:39:30 +0200

    Update departmentprofile.js

--- departmentprofile.js
+++ departmentprofile.js
@@ -1,24 +1,23 @@
-window.onload = function() {
-    var text = document.getElementsByClassName("input-field");
-    for (var i = 0; i < text.length; i++) {
-      text[i].disabled = true;
-    }
-    var textFields = document.getElementsByClassName("ssradio");
-    for (var i = 0; i < textFields.length; i++) {
-      textFields[i].disabled = true;
-    }
-  };
 
+ window.onload = function() {
+  var text = document.getElementsByClassName("input-field");
+  for (var i = 0; i < text.length; i++) {
+    text[i].disabled = true;
+  }
+  var textFields = document.getElementsByClassName("ssradio");
+  for (var i = 0; i < textFields.length; i++) {
+    textFields[i].disabled = true;
+  }
+};
+
+let urlParams;
 window.onload = function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const myParam = urlParams.get('id');
-    let stds = JSON.parse(localStorage.getItem("Students")) || [];  
-    for (var i = 0; i < stds.length; i++) {
-      if (myParam == stds[i].id) {
-  
-      //document.getElementById("output").setAttribute("src" , stds[i].photosrc);
-      console.log(stds[i].photosrc)
-      document.getElementById("Sname").value = stds[i].name;
+  urlParams = new URLSearchParams(window.location.search);
+  const myParam = urlParams.get('id');
+  let stds = JSON.parse(localStorage.getItem("Students")) || [];
+  for (var i = 0; i < stds.length; i++) {
+    if (myParam == stds[i].id) {
+      document.getElementById("Sname").innerText = stds[i].name;
       document.getElementById("Sid").value = stds[i].id;
       document.getElementById("Semail").value = stds[i].email;
       document.getElementById("Sgpa").value = stds[i].gpa;
@@ -26,35 +25,37 @@ window.onload = function() {
       document.getElementById("Saddress").value = stds[i].address;
       document.getElementById("Sphone").value = stds[i].phone;
       document.getElementById("Sdate").value = stds[i].date;
-      document.getElementById("Slevel").value = stds[i].level;
-      const department = stds[i]?.department; // use optional chaining to avoid errors if stds[i] is undefined or doesn't have a department property
-      const input = document.querySelector('input[name="Sdep"][value="' + department + '"]');
-        if (input) {
-          input.checked = true; // use checked property instead of selected for radio buttons
-        }
+      const level = stds[i]?.level; // use optional chaining to avoid errors if stds[i] is undefined or doesn't have a level property
+      const input = document.querySelector('input[name="Slevel"][value="' + level + '"]');
+      if (input) {
+        input.checked = true;
       }
+
+      const department = stds[i]?.department; // use optional chaining to avoid errors if stds[i] is undefined or doesn't have a department property
+      const select = document.getElementById("dep");
+      if (department) {
+          const options = select.options;
+          for (let j = 0; j < options.length; j++) {
+            if (options[j].value === department) {
+              select.value = department;
+              break;
+            }
+            }
+          }       
+                
     }
+  }
 };
+
 document.addEventListener("DOMContentLoaded", () => {
+  const saveButton = document.querySelector(".submit");
+  const selectElement = document.getElementById("dep");
 
-
-    const selectElement = document.getElementById("Sdep");
-    const saveButton = document.querySelector(".submit");
-  
+  if (selectElement) { // check if the element exists
     saveButton.addEventListener("click", () => {
       const selectedOption = selectElement.value;
-      console.log(selectedOption);
-        
-    });
-  });
-
-document.addEventListener("DOMContentLoaded", () => {
-    const saveButton = document.querySelector(".submit");
-    const selectElement = document.getElementById("Sdep");
-    
-    saveButton.addEventListener("click", () => {
       Swal.fire({
-        title: 'Selected department is (' + selectElement.value + ")",
+        title: 'Selected department is (' + selectedOption + ")",
         text: "Are you sure you want to save?",
         icon: 'question',
         showCancelButton: true,
@@ -62,31 +63,27 @@ document.addEventListener("DOMContentLoaded", () => {
         cancelButtonColor: '#00539F',
         confirmButtonText: 'Yes',
         cancelButtonText: 'No'
-
       }).then((result) => {
         if (result.isConfirmed) {
-          const selectedOption = selectElement.value;
-          console.log(selectedOption);
+          let stds = JSON.parse(localStorage.getItem("Students")) || [];
+          const myParam = urlParams.get('id');
+          for (var i = 0; i < stds.length; i++) {
+            if (myParam == stds[i].id) {
+              stds[i].department = selectedOption;
+              localStorage.setItem("Students", JSON.stringify(stds));
+            }
+          }
+          selectElement.value = selectedOption; // update the select element selection
+        } else {
+          const stds = JSON.parse(localStorage.getItem("Students")) || [];
+          const myParam = urlParams.get('id');
+          for (var i = 0; i < stds.length; i++) {
+            if (myParam == stds[i].id) {
+              selectElement.value = stds[i].department;
+            }
+          }
         }
       })
     });
+  }
 });
-  
-
-  // document.addEventListener("DOMContentLoaded", () => {
-  //   const saveButton = document.querySelector(".submit");
-  //   const selectElement = document.getElementById("Sdep");
-  //   saveButton.addEventListener("click", () => {
-  //     const confirmation = confirm("Selected department is (" + selectElement.value +") are you sure you want to save it?");
-      
-  //     if (confirmation == true) {
-  //       const selectedOption = selectElement.value;
-  //       console.log(selectedOption);
-      
-  //     } else {
-
-  //     }
-  //   });
-
-  // });
-  
